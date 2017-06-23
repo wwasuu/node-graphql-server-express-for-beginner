cbb5ae488c0e2f4e3e3d37d15cee14a30f1c6356
wasu <wasu@thinknet.co.th>
2017-06-23 14:44:30 +0700

    step1 using graphql.js only for understand concept

---
+++ server.js
@@ -0,0 +1,57 @@
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLFloat,
+} from 'graphql'
+
+import pokemonData from './data/pokemon.json'
+
+const queryPokemon = new GraphQLObjectType({
+  name: "pokemon",
+  fields: {
+    name: {
+      type: GraphQLString,
+    },
+    nameJP: {
+      type: GraphQLString,
+    },
+    height: {
+      type: GraphQLFloat,
+    },
+    weight: {
+      type: GraphQLFloat,
+    },
+  }
+}); 
+
+const queryType = new GraphQLObjectType({
+  name: "queryPokemon",
+  fields: {
+    pokemon: {
+      type: new GraphQLList(queryPokemon),
+      resolve() {
+        return pokemonData
+      }
+    }
+  }
+});
+
+const pokemonSchema = new GraphQLSchema({
+  query: queryType
+});
+
+var query = `{ 
+  pokemon {
+    name,
+    nameJP,
+    height,
+    weight
+  }
+}`;
+
+graphql(pokemonSchema, query).then(result => {
+  console.log(result.data);
+});
